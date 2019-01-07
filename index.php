f952cdb26ca3c6cf21fb3c05606e62c5cd80f066
stephbak <stephane.bakum@sfr.fr>
2019-01-07 10:25:09 +0100

    p4exe1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>php partie4</title>
+</head>
+<body>
+  <?php function boolean(){
+    return true;
+  }
+  ?>
+</body>
+</html>
